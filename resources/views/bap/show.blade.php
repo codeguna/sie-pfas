6cf4f1374803f1cebe8e6eac5eeaee02269f419e
codeguna <pratamagunadhi@gmail.com>
2022-12-13 14:58:45 +0700

    Add Header

--- resources/views/bap/show.blade.php
+++ resources/views/bap/show.blade.php
@@ -19,6 +19,7 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
+                            <h4>Daftar Fasilitas</h4>
                             @php
                                 $index = 0;
                             @endphp
